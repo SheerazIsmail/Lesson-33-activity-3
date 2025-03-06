9bb99b5bdd88c2c586f40479cc96f6589a62a071
SheerazIsmail <sheerazi539@gmail.com>
2025-03-06 20:29:36 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import getpass
+
+name = input("Enter your name: ")
+age = input("Enter you age: ")
+gender = input("Enter you gender: ")
+grade = input("Enter you grade: ")
+is_student = input("Are you a student? (True/False): ")
+height= input("Enter you height: ")
+username = input("Enter you username: ")
+password = getpass.getpass("Enter you password: ")
